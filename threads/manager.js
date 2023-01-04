d6164c820d36f1ffe61e40f9dcfcc6b43a663b61
bigdsiva <99091131+LilShadowBaka@users.noreply.github.com>
2023-01-04 15:51:32 -0500

    Update manager.js

--- threads/manager.js
+++ threads/manager.js
@@ -1,1 +1,33 @@
+// Import
+import {Weed} from '/threads/strain.js';
 
+// Export
+
+// Global Vars
+let plotRate = 1;
+let l = 0;
+let plotStrainNum = 0;
+
+
+// Strain declaration + index
+let weed = Weed(1, 1, 5, 10, 'img/x1', 'common');
+
+// Weed growth checker
+let grownStrains = [];
+
+function checkGrowth(weed) {
+    if(weed.growth === 10) {
+        grownStrains.push(weed);
+    }
+}
+
+let manager = {
+    managerlvl:0,
+    managerSec: 0,
+    harvest: function() {
+        for(let i; i <= grownStrains.length; i++) {
+            grownStrains.pop();
+        }
+    },
+
+}
